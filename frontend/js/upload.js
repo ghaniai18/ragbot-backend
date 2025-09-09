e633748964b1b070e6df976ce4c8c98699cc2a11
Ahmad Usman Ghani <usmanghani.ips@gmail.com>
2025-09-09 13:56:04 +0500

    Update upload.js

--- frontend/js/upload.js
+++ frontend/js/upload.js
@@ -1,4 +1,4 @@
-const baseURL = "http://127.0.0.1:8000";
+const baseURL = "https://your-app.onrender.com/login";
 let selectFile = null;         
 let currentFileBlobUrl = null;
 async function uploadFile() {
